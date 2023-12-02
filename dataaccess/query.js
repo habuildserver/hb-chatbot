4b0aa039a4d885840d6e41a07ddd432cad014a71
bhaveshabuild <bhavesh.pandey@habuild.in>
2023-12-02 12:26:45 +0530

    Revert "save chat details"
    This reverts commit b590eb02011ee8281b927ce10d9d82ed9ce844cb.

--- dataaccess/query.js
+++ dataaccess/query.js
@@ -1,7 +1,4 @@
-const INSERT_CHAT_DETAIL = `INSERT INTO memberchatdetails (id, mobilenumber, name, chatrequesttimestamp,
-                            whatsappmessageid, waticonversationid, question, answer, responder, waid, eventtype, watiserverid, inactive)
-                            VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13);`
 
-module.exports = {
-    INSERT_CHAT_DETAIL
+
+module.exports = {     
 };
--- webhook/webhookda.js
+++ webhook/webhookda.js
@@ -1,26 +1,7 @@
 const HBLogger = require(process.cwd() + '/utility/logger').logger;
-const dbHelper = require(process.cwd() + '/dataaccess/dbhelper');
-const { INSERT_CHAT_DETAIL } = require(process.cwd() + '/dataaccess/query');
-
-const processQueryWithFilters = async (query = '', filters = []) => {
-    const response = await dbHelper.executeNonQuery(query, filters);
-    return response;
-};
+const dbHelper = require(process.cwd() + '/dataaccess/dbhelper'); 
+const {} = require(process.cwd() + '/dataaccess/query');
 
 const webhookDa = () => {};
-
-webhookDa.addChatDetails = async (chatDetail) => {
-    HBLogger.info(`in addChatDetails call start`);
-    let queryResult = await processQueryWithFilters(
-        INSERT_CHAT_DETAIL,
-        [chatDetail?.id, chatDetail?.mobilenumber, chatDetail?.name, chatDetail?.chatrequesttimestamp,
-            chatDetail?.whatsappmessageid, chatDetail?.waticonversationid, chatDetail?.question, chatDetail?.answer,
-            chatDetail?.responder, chatDetail?.waid, chatDetail?.eventtype, chatDetail?.watiserverid, chatDetail?.inactive]
-    );
-    HBLogger.info(
-        `managementDa.addChatDetails call end queryResult ${JSON.stringify(queryResult?.rows)} `
-    )
-    return queryResult;
-}
 
 module.exports = { webhookDa };
