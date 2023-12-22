8abb61ea69907868f89d58e550e352509c72ca1b
bhaveshabuild <bhavesh.pandey@habuild.in>
2023-12-22 23:57:10 +0530

    remove kafka auth

--- queue/producer.js
+++ queue/producer.js
@@ -4,12 +4,12 @@ const HBLogger = require(process.cwd() + '/utility/logger').logger;
 const kafka = new Kafka({
     clientId: process.env.KAFKA_PRODUCER_CLIENT_ID,
     brokers: process.env.KAFKA_BROKER.split(','),
-    ssl: process.env.KAFKA_SSL,
-    sasl: {
-        mechanism: process.env.KAFKA_SASL_MECHANISM,
-        username: process.env.KAFKA_SASL_USERNAME,
-        password: process.env.KAFKA_SASL_PASSWORD
-    },
+    // ssl: process.env.KAFKA_SSL,
+    // sasl: {
+    //     mechanism: process.env.KAFKA_SASL_MECHANISM,
+    //     username: process.env.KAFKA_SASL_USERNAME,
+    //     password: process.env.KAFKA_SASL_PASSWORD
+    // },
 })
 
 const pushToQueue = async (topic, message) => {
